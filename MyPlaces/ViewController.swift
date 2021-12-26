ecdadb853d80e8010a0b9e6f672cb7c6793d132d
Dmitriy Grishechko <mitrio78@gmail.com>
2021-12-26 18:09:04 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Дмитрий Гришечко on 26.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
