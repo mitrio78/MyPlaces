4679fde3da13e5bf834288cebbd2bee4daee02b4
Dmitriy Grishechko <mitrio78@gmail.com>
2022-01-04 15:59:43 +0300

    Custom Table View

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Дмитрий Гришечко on 04.01.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeOfLabel: UILabel!
+    
+    
+    
+    
+
+
+
+}
